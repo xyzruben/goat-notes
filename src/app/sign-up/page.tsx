f679c285d8d8d302e1fb62bfef5d4a16d7ebd6de
Ruben Rivas <Ruben@Rubens-MacBook-Pro.local>
2025-05-13 10:58:18 -0700

    Initial commit

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,20 @@
+import AuthForm from "@/components/ui/AuthForm"
+import { Card, CardHeader, CardTitle } from "@/components/ui/card"
+
+function SignUpPage() {
+  return (
+    <div
+    className="mt-20 flex flex-1 flex-col items-center">
+      <Card className="w-full max-w-md">
+        <CardHeader className="mb-4">
+          <CardTitle className="text-center text-3xl">SignUp</CardTitle>
+
+        </CardHeader>
+        <AuthForm type="signUp" />
+
+      </Card>
+    </div>
+  )
+}
+
+export default SignUpPage
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,53 @@
+import { shadow } from "../../app/styles/utiles"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./button";
+import DarkModeToggle from "./DarkModeToggle"
+import LogOutButton from "./LogOutButton";
+import { getUser } from "@/auth/server";
+
+async function Header() {
+    const user = await getUser();
+
+    return (
+        <header className="relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8"
+        style={{
+            boxShadow: shadow
+        }}
+        
+        >
+            <Link className="flex items-end gap-2" href="/">
+                <Image 
+                    src="/goatius.png" 
+                    height={60} 
+                    width={60} 
+                    alt="logo" 
+                    className="rounded-full" 
+                    priority
+                    />
+
+                <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">
+                    GOAT <span>Notes</span>    
+                </h1>    
+
+            </Link>
+            <div className="flex gap-4">
+                {user ? (
+                    <LogOutButton />
+                ) : (
+                    <>
+                    <Button asChild>
+                        <Link href="/sign-up" className="hidden sm:block">Sign Up</Link>
+                    </Button>
+                    <Button asChild variant="outline">
+                        <Link href="/login">Login</Link>
+                    </Button>
+                    </>
+                )}
+                <DarkModeToggle />
+            </div>
+        </header>
+    )
+}
+
+export default Header
