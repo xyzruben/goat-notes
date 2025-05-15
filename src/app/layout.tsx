e7808816edc5ada16bfc833ac72f9bce830830e0
Ruben Rivas <xyzruben10@gmail.com>
2025-05-14 18:51:53 -0700

    Implement responsive sidebar layout, update global styles, and add reusable UI components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./styles/globals.css";
 import { ThemeProvider } from "../providers/ThemeProvider"
 import { Toaster } from "../components/ui/sonner"
 import Header from "@/components/ui/Header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/ui/AppSidebar";
 
 
 export const metadata: Metadata = {
@@ -24,10 +26,13 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <div className="flex min-h-screen w-full flex-col">
+            <SidebarProvider>
+              <AppSidebar />
+              <div className="flex min-h-screen w-full flex-col">
               <Header/>
               <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">{children}</main>
-            </div>
+              </div>
+            </SidebarProvider>
             <Toaster />
           </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 function HomePage() {
-  return <div className="h-20 bg-red-400 text-blue-300">HomePage</div>
+  return <div className="h-20 bg-blue-900 text-blue-300">HomePage</div>
 }
 
 export default HomePage;
