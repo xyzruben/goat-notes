d092409683f959808b8c0dabcf93b72e6ff8f95e
Ruben Rivas <Ruben@Rubens-MacBook-Pro.local>
2025-05-15 21:50:24 -0700

    Fix SidebarGroupContent import and TypeScript props error by switching to default export

--- src/components/ui/SidebarGroupContent.tsx
+++ src/components/ui/SidebarGroupContent.tsx
@@ -4,12 +4,13 @@ import { Note } from "@prisma/client";
 
 type Props = {
     notes: Note[];
-}
+};
 
-function SidebarGroupContent() {
+function SidebarGroupContent( {notes}: Props ) {
+  console.log(notes);
   return (
-    <div>SidebarGroupContent</div>
+    <div>Your notes here</div>
   )
 }
 
-export default SidebarGroupContent
+export default SidebarGroupContent;
