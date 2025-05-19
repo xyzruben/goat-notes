51c7373cb365a34f800dd3d500d354aabba6072a
Ruben Rivas <Ruben@Rubens-MacBook-Pro.local>
2025-05-19 15:26:57 -0700

    Implemented note text area input, actions, and constants; updated layout and buttons.

--- src/components/ui/NewNoteButton.tsx
+++ src/components/ui/NewNoteButton.tsx
@@ -1,16 +1,25 @@
 "use client";
 
 import { User } from "@supabase/supabase-js";
+import { Button } from "./button";
+import { Loader2 } from "lucide-react";
 
 type Props = {
     user: User | null;
 }
 
 function NewNoteButton({ user }: Props) {
-    console.log(user?.email)
-  return (
-    <div>NewNoteButton</div>
-  )
+    console.log(user?.email);
+  return(<Button 
+  onClick={handleClickNewNoteButton} 
+  variant="secondary" 
+  className="w-24" 
+  disabled={loading}
+
+  >
+    {loading ? <Loader2 className="animate-spin" /> : "New Note"}
+  </Button>
+  );
 }
 
-export default NewNoteButton
+export default NewNoteButton;
