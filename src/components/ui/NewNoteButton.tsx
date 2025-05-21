cc6301223283c8ff238699cba45fb80e895cf6ee
Ruben Rivas <xyzruben10@gmail.com>
2025-05-20 18:28:43 -0700

    Made New note button functional and update note creation logic

--- src/components/ui/NewNoteButton.tsx
+++ src/components/ui/NewNoteButton.tsx
@@ -3,14 +3,44 @@
 import { User } from "@supabase/supabase-js";
 import { Button } from "./button";
 import { Loader2 } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
+import { useToast } from "@/hooks/use-toast";
+import { createNoteAction } from "@/actions/notes";
 
 type Props = {
     user: User | null;
-}
+};
 
 function NewNoteButton({ user }: Props) {
-    console.log(user?.email);
-  return(<Button 
+    const router = useRouter();
+
+    const {toast} = useToast()
+
+    const [loading, setLoading] = useState(false);
+
+    const handleClickNewNoteButton = async () => {
+      if (!user) {
+        router.push("/login")
+      } else {
+        setLoading(true)
+
+        const uuid = uuidv4()
+        await createNoteAction(uuid)
+        router.push(`/?noteId=${uuid}`)
+
+        toast({
+          title: "New Note Created",
+          description: "You have created a new note",
+          variant: "success"
+        });
+        setLoading(false)
+      }
+    }
+
+  return(
+  <Button 
   onClick={handleClickNewNoteButton} 
   variant="secondary" 
   className="w-24" 
