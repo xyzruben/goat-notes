d3473d05b988ea054f7cb156502788e0d7a79d0c
Ruben Rivas <Ruben@Rubens-MacBook-Pro.local>
2025-05-20 18:28:43 -0700

    Made New note button functional and update note creation logic

--- src/actions/notes.ts
+++ src/actions/notes.ts
@@ -4,10 +4,34 @@ import { getUser } from "@/auth/server";
 import { prisma } from "@/db/prisma";
 import { handleError } from "@/lib/utils";
 
+export const createNoteAction = async (noteId: string) => {
+    try {
+
+        const user = await getUser();
+
+        if (!user) throw new Error("You must be logged in to create a note.");
+
+        await prisma.note.create({
+            data: {
+                id: noteId,
+                authorId: user.id,
+                text: ""
+            },
+
+        })
+       
+
+        return {errorMessage : null};
+
+    } catch(error) {
+        return handleError(error)
+    }
+}
+
 export const updateNoteAction = async (noteId: string, text: string) => {
     try {
 
-        const user = getUser();
+        const user = await getUser();
 
         if (!user) throw new Error("You must be logged in to update a note.");
 
