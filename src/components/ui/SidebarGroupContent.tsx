ad23535d72867f9c006b3f40e0341a8a0dd25a39
Ruben Rivas <Ruben@Rubens-MacBook-Pro.local>
2025-05-21 22:06:12 -0700

    Installed Fuse.js and updated SidebarGroupContent for fuzzy search

--- src/components/ui/SidebarGroupContent.tsx
+++ src/components/ui/SidebarGroupContent.tsx
@@ -1,15 +1,48 @@
 "use client";
 
 import { Note } from "@prisma/client";
+import { SidebarGroupContent as SidebarGroupContentShadCN, SidebarMenu } from "./sidebar";
+import { SearchIcon } from "lucide-react";
+import { Input } from "./input";
+import { useEffect, useMemo, useState } from "react";
+import Fuse from "fuse.js"
 
 type Props = {
     notes: Note[];
 };
 
 function SidebarGroupContent( {notes}: Props ) {
-  console.log(notes);
+  const [searchText, setSearchText] = useState("");
+  const [localNotes, setLocalNotes] = useState(notes);
+
+  useEffect(() => {
+    setLocalNotes(notes)
+  }, [notes])
+
+  const fuse = useMemo(() => {
+    return new Fuse(localNotes, {
+      keys: ["text"],
+      threshold: .4
+    })
+  }, [localNotes])
+
+  const filteredNotes = searchText ? fuse.search(searchText).map(result => result.item) : localNotes;
+
   return (
-    <div>Your notes here</div>
+    <SidebarGroupContentShadCN>
+      <div className="relative flex items-center">
+        <SearchIcon className="absolute left-2 size-4" />
+        <Input
+        className="bg-muted pl-8" 
+        placeholder="Search your notes..."
+        value={searchText} 
+        onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+      <SidebarMenu className="mt-4">
+
+      </SidebarMenu>
+    </SidebarGroupContentShadCN>
   )
 }
 
