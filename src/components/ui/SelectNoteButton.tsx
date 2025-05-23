7b631323390c1600bf16e3fdf96eadebc86e4c8f
Ruben Rivas <xyzruben10@gmail.com>
2025-05-22 21:38:54 -0700

    added DeleteNoteButtons and SelectNoteButton components, updated SidebarGroupContent with Fuse.js logic.

---
+++ src/components/ui/SelectNoteButton.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { Note } from "@prisma/client";
+
+type Props = {
+    note: Note;
+};
+
+function SelectNoteButton({ note }: Props) {
+  return (
+    <div>SelectNoteButton</div>
+  )
+}
+
+export default SelectNoteButton;
