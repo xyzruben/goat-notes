67105863e5029c6d5085668cc345e7e1bf542219
Ruben Rivas <xyzruben10@gmail.com>
2025-05-24 16:00:02 -0700

    Refactor note selection and deletion logic, update SidebarGroupContent, and add alert dialog component

--- src/components/ui/DeleteNoteButton.tsx
+++ src/components/ui/DeleteNoteButton.tsx
@@ -1,4 +1,20 @@
-'use client'
+"use client"
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "./button";
+import { Loader2, Trash2 } from "lucide-react";
+import { useTransition } from "react";
+
 
 type Props = {
     noteId: string;
@@ -6,7 +22,40 @@ type Props = {
 };
 
 function DeleteNoteButton({ noteId, deleteNoteLocally }: Props) {
-  return <div>DeleteNoteButton</div>
+
+  const [isPending, startTransition] = useTransition();
+
+  const handleDeleteNote = () => {}
+
+  return (
+    <AlertDialog>
+  <AlertDialogTrigger asChild>
+    <Button 
+    className="absolute right-2 top-1/2 -translate-y-1/2 size-7 p-0 opacity-0 group-hover/item:opacity-100 [&_svg]:size-4" 
+    variant="ghost"
+    ><Trash2 />
+    </Button>
+  </AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>Are you sure you want to delete this note?</AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. This will permanently delete your Note
+        and remove your data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction onClick={handleDeleteNote} 
+      className="bg-destructive text-destructive-foreground hover:bg-destructive/90 w-24"
+      >
+        {isPending ? <Loader2 className="animate-spin" /> : "Delete"}
+      </AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
+
+  )
   
 }
 
--- src/components/ui/SelectNoteButton.tsx
+++ src/components/ui/SelectNoteButton.tsx
@@ -3,7 +3,9 @@
 import useNote from "@/hooks/useNote";
 import { Note } from "@prisma/client";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { SidebarMenuButton } from "./sidebar";
+import Link from "next/link";
 
 type Props = {
     note: Note;
@@ -13,18 +15,41 @@ function SelectNoteButton({ note }: Props) {
   const noteId = useSearchParams().get("noteId") || "";
 
   const {noteText: selectedNoteText} = useNote();
-  const [shouldUseGlobalNoteText, setShouldUseGlobalNoteText] = useState(false)
-  const [localNoteText, setLocalNoteText] = useState(note.text)
+  const [shouldUseGlobalNoteText, setShouldUseGlobalNoteText] = useState(false);
+  const [localNoteText, setLocalNoteText] = useState(note.text);
+
+  useEffect(() => {
+    if (noteId === note.id) {
+      setShouldUseGlobalNoteText(true)
+    } else {
+      setShouldUseGlobalNoteText(false)
+    }
+  }, [noteId, note.id]);
+
+  useEffect(() => {
+    if (shouldUseGlobalNoteText) {
+      setLocalNoteText(selectedNoteText);
+    }
+  }, [selectedNoteText, shouldUseGlobalNoteText]); 
+
 
   const blankNoteText = "EMPTY NOTE";
   let noteText = localNoteText || blankNoteText;
-  if (shouldBeGlobalNoteText) {
+  if (shouldUseGlobalNoteText) {
     noteText = selectedNoteText || blankNoteText;
   }
 
   return (
-    <div>SelectNoteButton</div>
-  )
+  <SidebarMenuButton
+   asChild 
+   className={`items-start gap-0 pr-12 ${note.id === noteId && "bg-sidebar-accent/50"}`}>
+    <Link href={`/?noteId=${note.id}`} className="flex h-fit flex-col">
+    <p className="w-full overflow-hidden truncate text-eclipsis whitespace-nowrap">
+      {noteText}      
+    </p>
+    <p className="text-muted-foreground text-xs ">{note.updatedAt.toLocaleDateString()}</p>
+    </Link>
+  </SidebarMenuButton>);
 }
 
 export default SelectNoteButton;
